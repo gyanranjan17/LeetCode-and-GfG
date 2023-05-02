2d577a1d8216a909e0870787ae1b736fb25abc72
GYAN RANJAN MOHANTY <81802413+gyanranjan17@users.noreply.github.com>
2023-05-03 00:17:18 +0530

    Added solution - LeetHub

---
+++ Number of Provinces - GFG/number-of-provinces.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    void bfs(vector<vector<int>>&adj,vector<int> &vis, int node){
+    vis[node]=1;
+    queue<int> q;
+    q.push(node);
+    while(!q.empty()){
+        int Node=q.front();
+        q.pop();
+        for(int i=0;i<adj[Node].size();i++){
+            if(vis[i]==0 && adj[Node][i]==1){
+                vis[i]=1;
+                q.push(i);
+            }
+        }
+    }
+    
+} 
+     
+    int numProvinces(vector<vector<int>> adj, int V) {
+        // code here
+        int count=0;
+        vector<int> vis(V,0);
+        for(int i=0;i<V;i++){
+            if(vis[i]==0){
+                count++;
+                bfs(adj,vis,i);
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int V,x;
+        cin>>V;
+        
+        vector<vector<int>> adj;
+        
+        for(int i=0; i<V; i++)
+        {
+            vector<int> temp;
+            for(int j=0; j<V; j++)
+            {
+                cin>>x;
+                temp.push_back(x);
+            }
+            adj.push_back(temp);
+        }
+        
+
+        Solution ob;
+        cout << ob.numProvinces(adj,V) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
