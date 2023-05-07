9d59249d32ff074553b208b5c4fb238486fbc859
GYAN RANJAN MOHANTY <81802413+gyanranjan17@users.noreply.github.com>
2023-05-07 08:21:53 +0530

    Added solution - LeetHub

--- Articulation Point - I - GFG/articulation-point-i.cpp
+++ Articulation Point - I - GFG/articulation-point-i.cpp
@@ -12,14 +12,14 @@ using namespace std;
 class Solution {
   public:
     int timer = 1;
-    void dfs(int node, int parent, vector<int> &vis, int tin[], int low[],
+    void dfs(int node, int parent, vector<int> &vis, vector<int> &tin, vector<int> &low,
              vector<int> &mark, vector<int>adj[]) {
         vis[node] = 1;
         tin[node] = low[node] = timer;
         timer++;
         int child = 0;
         for (auto it : adj[node]) {
-            if (it == parent) continue;
+            // if (it == parent) continue;
             if (!vis[it]) {
                 dfs(it, node, vis, tin, low, mark, adj);
                 low[node] = min(low[node], low[it]);
@@ -39,12 +39,10 @@ class Solution {
 public:
     vector<int> articulationPoints(int n, vector<int>adj[]) {
         vector<int> vis(n, 0);
-        int tin[n];
-        int low[n];
+        vector<int> tin(n);
+        vector<int> low(n);
         vector<int> mark(n, 0);
-                dfs(0, -1, vis, tin, low, mark, adj);
-            
-        
+        dfs(0, -1, vis, tin, low, mark, adj);
         vector<int> ans;
         for (int i = 0; i < n; i++) {
             if (mark[i] == 1) {
