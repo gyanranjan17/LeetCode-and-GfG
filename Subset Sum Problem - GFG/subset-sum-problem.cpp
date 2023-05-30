b88119080e87bdd7246414c7e591e64b954a6ec5
GYAN RANJAN MOHANTY <81802413+gyanranjan17@users.noreply.github.com>
2023-05-30 19:39:27 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -9,22 +9,42 @@ using namespace std;
 
 class Solution{   
 public:
-    bool f(int i,int sum,vector<int>&arr,vector<vector<int>> &dp){
-        if(sum==0) return dp[i][sum]=1;
-        if(i==0) return dp[i][sum]=(sum==arr[i]);
-        if(dp[i][sum]!=-1) return dp[i][sum];
-        int nt=f(i-1,sum,arr,dp);
-        int t=0;
-        if(sum>=arr[i])
-        t=f(i-1,sum-arr[i],arr,dp);
-        return dp[i][sum]=(nt | t);
+    // bool f(int i,int sum,vector<int>&arr,vector<vector<int>> &dp){
+    //     if(sum==0) return dp[i][sum]=1;
+    //     if(i==0) return dp[i][sum]=(sum==arr[i]);
+    //     if(dp[i][sum]!=-1) return dp[i][sum];
+    //     int nt=f(i-1,sum,arr,dp);
+    //     int t=0;
+    //     if(sum>=arr[i])                                     
+    //     t=f(i-1,sum-arr[i],arr,dp);
+    //     return dp[i][sum]=(nt | t);
         
-    }
+    // }
     
     bool isSubsetSum(vector<int>arr, int sum){
         int n=arr.size();
-        vector<vector<int>> dp(n+1,vector<int>(sum+1,-1));
-        return f(n-1,sum,arr,dp);
+        vector<vector<bool>> dp(n,vector<bool>(sum+1,0));
+        for(int i=0;i<n;i++){
+            dp[i][0]=1;
+        }
+        dp[0][arr[0]]=1;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<sum+1;j++){
+                    int nt=dp[i-1][j];
+                    int t=0;
+                    if(j>=arr[i])                         //    Space optimisation will be done
+                    t=dp[i-1][j-arr[i]];
+                    dp[i][j]=(nt || t);
+                
+            }
+        }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<sum+1;j++){
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        return dp[n-1][sum];
     }
 };
 
