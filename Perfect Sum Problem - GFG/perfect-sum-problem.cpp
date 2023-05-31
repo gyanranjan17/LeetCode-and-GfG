020a10fcfa4072e333f2d897921e5fc8a0597a25
GYAN RANJAN MOHANTY <81802413+gyanranjan17@users.noreply.github.com>
2023-05-31 16:33:05 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -7,20 +7,6 @@ class Solution{
 
 	public:
 	int mod=1e9+7;
-    int f(int i,int sum,int arr[],vector<vector<int>> &dp){
-        if(i==0) {
-            if(arr[i]==0 && sum==0) return 2;
-            if(sum==0 || arr[0]==sum) return 1;
-            return 0;
-        }
-        
-        if(dp[i][sum]!=-1) return dp[i][sum];
-        int np=f(i-1,sum,arr,dp)%mod;
-        int p=0;
-        if(arr[i]<=sum)
-        p=f(i-1,sum-arr[i],arr,dp)%mod;
-        return dp[i][sum]=(np+p)%mod;
-    }
 	int perfectSum(int arr[], int n, int sum)
 	{
         // Your code goes here
