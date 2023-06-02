97bc75b44c8803936f4d01432dc111f9679014b9
GYAN RANJAN MOHANTY <81802413+gyanranjan17@users.noreply.github.com>
2023-06-02 18:20:20 +0530

    Added solution - LeetHub

--- Target Sum - GFG/target-sum.cpp
+++ Target Sum - GFG/target-sum.cpp
@@ -10,41 +10,33 @@ using namespace std;
 
 class Solution {
   public:
-    int f(vector<int>& arr, int n, int sum)
-	{
-        // Your code goes here
-        vector<vector<int>> dp(n,vector<int> (sum+1,-1));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<sum+1;j++){
-                if(i==0){
-                    if(arr[i]==0 && j==0) dp[i][j]= 2;
-                    else if(j==0 || arr[0]==j) dp[i][j]= 1;
-                    else dp[i][j]= 0;
-                }
-                else{
-                    int np=dp[i-1][j];
-                    int p=0;
-                    if(arr[i]<=j)
-                    p=dp[i-1][j-arr[i]];
-                    dp[i][j]=(np+p);
-                }
-            }
+    int f(int i,int t,vector<int>& nums,vector<vector<int>> &dp){
+        if(i==0){
+            if(nums[i]==0 && t==0) return 2;
+            if(t==0 || nums[0]==t) return 1;
+            return 0;
         }
-        return dp[n-1][sum];
-        
-	}
-    int countPartitions(int n, int d, vector<int>& arr) {
-        // Code here
-        int s=0;
-        for(int i=0;i<n;i++)
-        s+=arr[i];
-        if(s-d>=0 && (s-d)%2==0)
-        return f(arr,n,(s-d)/2);
-        return 0;
+        if(dp[i][t]!=-1)return dp[i][t];
+        int np=f(i-1,t,nums,dp);
+        int p=0;
+        if(nums[i]<=t)
+        p=f(i-1,t-nums[i],nums,dp);
+        return dp[i][t]=p+np;
+
     }
     int findTargetSumWays(vector<int>&nums ,int target) {
         //Your code here
-        return countPartitions(nums.size(),target,nums);
+        int n=nums.size(),s=0;
+        // vector<vector<int>> dp(n,vector<int>((s-target)/2+1,-1));
+        
+        for(int i=0;i<n;i++){
+            s+=nums[i];
+        }
+        if(s-target>=0 && (s-target)%2==0){
+        vector<vector<int>> dp(n,vector<int>((s-target)/2+1,-1));
+        return f(n-1,(s-target)/2,nums,dp);
+        }
+        return 0;
     }
 };
 
