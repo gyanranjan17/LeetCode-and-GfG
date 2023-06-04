4e1acdbf95987971153ffa28917ad65bb4d88a8d
GYAN RANJAN MOHANTY <81802413+gyanranjan17@users.noreply.github.com>
2023-06-04 15:30:50 +0530

    Added solution - LeetHub

---
+++ Row with minimum number of 1's - GFG/row-with-minimum-number-of-1s.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int minRow(int N, int M, vector<vector<int>> A) {
+        // code here
+        int ans=1e9,res;
+        for(int i=0;i<N;i++){
+            int o=0;
+            for(int j=0;j<M;j++){
+                if(A[i][j]==1)
+                o++;
+            }
+            if(o<ans){
+            res=i+1;
+            ans=o;
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, M;
+        cin >> N >> M;
+        vector<vector<int>> A(N, vector<int>(M));
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                cin >> A[i][j];
+            }
+        }
+        Solution ob;
+        cout << ob.minRow(N, M, A) << "\n";
+    }
+}
+// } Driver Code Ends
