23275ba787e73e9b0eb33ff3dfd2275f272153dc
GYAN RANJAN MOHANTY <81802413+gyanranjan17@users.noreply.github.com>
2023-08-19 17:55:17 +0530

    Added solution - LeetHub

---
+++ Knight Walk - GFG/knight-walk.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+	int minStepToReachTarget(vector<int>&k, vector<int>&t, int n){
+	    // Code here
+	    vector<vector<int>> vis(n+1,vector<int>(n+1,0));
+	    queue<pair<pair<int,int>,int>> q;
+	    q.push({{k[0],k[1]},0});
+	    int dr[]={1,1,-1,-1,2,2,-2,-2};
+	    int dc[]={2,-2,2,-2,1,-1,1,-1};
+	    vis[k[0]][k[1]]=1;
+	    while(!q.empty()){
+	            int r=q.front().first.first;
+	            int c=q.front().first.second;
+	            int st=q.front().second;
+	            if(t[0]==r && t[1]==c) return st;
+	            q.pop();
+	            for(int i=0;i<8;i++){
+	                int nr=r+dr[i];
+	                int nc=c+dc[i];
+	                if(nr>0 && nc>0 && nr<=n && nc<=n && !vis[nr][nc]){
+	                    vis[nr][nc]=1;
+	                    q.push({{nr,nc},st+1});
+	                }
+	            }
+	    }
+	    return -1;
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		vector<int>KnightPos(2);
+		vector<int>TargetPos(2);
+		int N;
+		cin >> N;
+		cin >> KnightPos[0] >> KnightPos[1];
+		cin >> TargetPos[0] >> TargetPos[1];
+		Solution obj;
+		int ans = obj.minStepToReachTarget(KnightPos, TargetPos, N);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
