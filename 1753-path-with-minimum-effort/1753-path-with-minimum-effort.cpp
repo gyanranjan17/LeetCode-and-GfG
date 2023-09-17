39f4ae4991cba175782ea373423be981ce706ed2
GYAN RANJAN MOHANTY <81802413+gyanranjan17@users.noreply.github.com>
2023-09-17 23:37:11 +0530

    Time: 110 ms (68.75%), Space: 20.3 MB (36.54%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -2,32 +2,29 @@ class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& h) {
         int n=h.size(),m=h[0].size();
-        queue<pair<pair<int,int>,int>> q;
-        int d[]={-1,0,1,0,-1};
-        q.push({{0,0},0});
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
+        pq.push({0,{0,0}});
         vector<vector<int>> eff(n,vector<int>(m,1e9));
+        int d[]={-1,0,1,0,-1};
         eff[0][0]=0;
-        int ans=1e9;
-        while(!q.empty()){
-            int x=q.front().first.first;
-            int y=q.front().first.second;
-            int dist=q.front().second;
-            q.pop();
-            if(x==n-1 && y==m-1) {
-                ans=min(ans,dist);
-                continue;
-            }
+        while(!pq.empty()){
+            int ef=pq.top().first;
+            int r=pq.top().second.first;
+            int c=pq.top().second.second;
+            pq.pop();
+            if(r==n-1 && c==m-1) return ef;
             for(int k=0;k<4;k++){
-                int nr=x+d[k];
-                int nc=y+d[k+1];
+                int nr=r+d[k];
+                int nc=c+d[k+1];
                 if(nr>=0 && nc>=0 && nr<n && nc<m){
-                    if(eff[nr][nc]>max(dist,abs(h[nr][nc]-h[x][y]))){
-                        eff[nr][nc]=max(dist,abs(h[nr][nc]-h[x][y]));
-                        q.push({{nr,nc},eff[nr][nc]});
+                    int nef=max(ef,abs(h[r][c]-h[nr][nc]));
+                    if(eff[nr][nc]>nef){
+                        eff[nr][nc]=nef;
+                        pq.push({nef,{nr,nc}});
                     }
                 }
             }
         }
-        return ans;
+        return 0;
     }
 };
