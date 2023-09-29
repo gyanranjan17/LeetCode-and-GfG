87156685a06e01a45336c1f59ff209079f4bbd48
GYAN RANJAN MOHANTY <81802413+gyanranjan17@users.noreply.github.com>
2023-09-29 12:29:54 +0530

    Time: 118 ms (73.39%), Space: 96.7 MB (22.05%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.cpp
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
-        bool in=1,ds=1;
+        bool in=0,ds=0;
         for(int i=1;i<nums.size();i++){
-            if(nums[i]>nums[i-1]) ds=0;
-            if(nums[i] <nums[i-1]) in=0;
-            if(!in && !ds) return 0;
+            if(nums[i]>nums[i-1]) in=1;
+            if(nums[i] <nums[i-1]) ds=1;
+            if(in && ds) return 0;
         }
         return 1;
     }
