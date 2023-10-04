4df95c4460fb06f4fd70393857a3bd03f4e22ae2
GYAN RANJAN MOHANTY <81802413+gyanranjan17@users.noreply.github.com>
2023-10-04 17:52:12 +0530

    Time: 111 ms (58.09%), Space: 208.5 MB (14.05%) - LeetHub

---
+++ 0817-design-hashmap/0817-design-hashmap.cpp
@@ -0,0 +1,27 @@
+class MyHashMap {
+public:
+    int mapp[1000001];
+    MyHashMap() {
+        fill(mapp,mapp+1000000,-1);
+    }
+    
+    void put(int key, int value) {
+        mapp[key]=value;
+    }
+    
+    int get(int key) {
+        return mapp[key];
+    }
+    
+    void remove(int key) {
+        mapp[key]=-1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
