cead326e0caa9ad7519afb1f0703c3660bdd8ea0
GYAN RANJAN MOHANTY <81802413+gyanranjan17@users.noreply.github.com>
2023-10-15 17:21:39 +0530

    Time: 0 ms (100%), Space: 6.6 MB (100%) - LeetHub

---
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/3150-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        string ans="";
+        int i=0,j=0,c=0,len=1e9;
+        pair<int,int> p={-1,-1};
+        while(j<s.size()){
+            if(s[j]=='1') c++;
+            
+            while(c>=k){
+                if(c==k){
+                    if(j-i+1<len){
+                        len=j-i+1;
+                        ans=string(s.begin()+i,s.begin()+j+1);
+                    }
+                    else if(j-i+1==len){
+                        string temp=string(s.begin()+i,s.begin()+j+1);
+                        if(ans>temp)
+                        ans=temp;
+                    }
+                }
+                if(s[i]=='1')
+                    c--;
+                
+                i++;
+            }
+            j++;
+        }
+        return ans;
+    }
+};
