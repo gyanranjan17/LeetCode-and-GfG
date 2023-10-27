0444685f07936791de27d4b399ef2682450ee068
GYAN RANJAN MOHANTY <81802413+gyanranjan17@users.noreply.github.com>
2023-10-28 00:02:41 +0530

    Added solution - LeetHub

--- Minimum Deletions - GFG/minimum-deletions.cpp
+++ Minimum Deletions - GFG/minimum-deletions.cpp
@@ -8,18 +8,23 @@ using namespace std;
 
 class Solution{
   public:
-    int lcs(string &a,string &b,int m,int n,vector<vector<int>> &dp){
-        if(!m or !n) return 0;
-        if(dp[m][n]!= -1) return dp[m][n]; 
-        if(a[m-1] == b[n-1]) return dp[m][n]=1+lcs(a,b,m-1,n-1,dp);
-        return dp[m][n]=max(lcs(a,b,m-1,n,dp),lcs(a,b,m,n-1,dp));
+    int lcs(int i,int j,string &a,string &s,vector<vector<int>> &dp){
+        int n=a.size();
+        if(i>=n || j>=n) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(a[i]==s[j]){
+            return dp[i][j]= 1+lcs(i+1,j+1,a,s,dp);
+        }
+        else{
+            return dp[i][j]=max(lcs(i+1,j,a,s,dp),lcs(i,j+1,a,s,dp));
+        }
     }
-    int minimumNumberOfDeletions(string S) { 
-        string a = S;
-        int n = a.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        reverse(S.begin(),S.end());
-        return n-lcs(a,S,n,n,dp);
+    int minimumNumberOfDeletions(string s) { 
+        vector<vector<int>> dp(s.size(),vector<int>(s.size(),-1));
+        string a=s;
+        int ans=0;
+        reverse(s.begin(),s.end());
+        return s.size()-lcs(0,0,a,s,dp);
     } 
 };
 
